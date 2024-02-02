786d1bee39549f083df72e1241d2b7a17675f1dd
mac <apple_yinhai@icloud.com>
2024-02-02 16:25:57 +0800

    2

--- js.js
+++ js.js
@@ -261,7 +261,7 @@ function createtime() {
     if (String(snum).length == 1) {
         snum = "0" + snum;
     }
-    document.getElementById("timeDate").innerHTML = "️当前版本 1.2--本站已运行" + dnum + "天";
+    document.getElementById("timeDate").innerHTML = "️当前版本 1.1--本站已运行" + dnum + "天";
     document.getElementById("times").innerHTML = hnum + "小时" + mnum + "分" + snum + "秒";
 }
 
